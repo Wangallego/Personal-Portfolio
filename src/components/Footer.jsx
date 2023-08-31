03177428990087cb81e0347512b4f7c512692991
WanDev <128651125+Wangallego@users.noreply.github.com>
2023-08-31 08:52:43 +0200

    Update Footer.jsx links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,15 +12,15 @@ function Footer() {
         <ul className="flex justify-center items-center space-x-6">
           <li className="flex space-x-2 items-center">
             <img src={twitter} alt="" className="bg-white rounded-full p-1 w-[1.8rem]" />
-            <a href="#" target="_blank" className="text-white">Twitter</a>
+            <a href="https://twitter.com/home" target="_blank" className="text-white">Twitter</a>
           </li>
           <li className="flex space-x-2 items-center">
             <img src={gitHub} alt="" className="bg-white rounded-full p-1 w-[1.8rem]" />
-            <a href="#" target="_blank" className="text-white">GitHub</a>
+            <a href="https://github.com/Wangallego" target="_blank" className="text-white">GitHub</a>
           </li>
           <li className="flex space-x-2 items-center">
             <img src={linkedin} alt="" className="bg-white rounded-full p-1 w-[1.8rem]" />
-            <a href="#" target="_blank" className="text-white">Linkedin</a>
+            <a href="https://www.linkedin.com/in/juan-basoa-37bab0253/" target="_blank" className="text-white">Linkedin</a>
           </li>
         </ul>
       </div>
